b2a9a0f4a02a59a1939e05370858bb77d99b4251
Rafael Gioffi <rafaelgioffi@gmail.com>
2025-08-12 23:58:31 -0300

    Implementando Jwt

--- DevFreela.Core/Entities/User.cs
+++ DevFreela.Core/Entities/User.cs
@@ -2,7 +2,7 @@
 {
     public class User : BaseEntity
     {
-        public User(string fullName, string email, DateTime birthDate) : base()
+        public User(string fullName, string email, DateTime birthDate, string password, string role) : base()
         {
             FullName = fullName;
             Email = email;
@@ -13,12 +13,16 @@
             OwnedProjects = [];
             FreelanceProjects = [];
             Comments = [];
+            Password = password;
+            Role = role;
         }
 
         public string FullName { get; private set; }
         public string Email { get; private set; }
         public DateTime BirthDate { get; private set; }
         public bool Active { get; private set; }
+        public string Password { get; private set; }
+        public string Role { get; private set; }
         public List<UserSkill> Skills { get; private set; }
         public List<Project> OwnedProjects { get; private set; }
         public List<Project> FreelanceProjects { get; private set; }
